175e6b367e2e6d8c20b8484d1d6e66e8908e221c
eugsa <eugenie.saliou@gmail.com>
2024-07-02 18:35:56 +0200

    Add book titles in a dataframe

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from bs4 import BeautifulSoup
 URL = "https://www.goodreads.com/shelf/show/non-fiction"
 
 def main():
+  # dfs = pd.read_html(URL)
+  # print(dfs)
+
   page = urlopen(URL)
   html_bytes = page.read()
   html = html_bytes.decode("utf-8")
@@ -12,8 +15,14 @@ def main():
   soup = BeautifulSoup(html, "html.parser")
   print(soup.title.string)
 
-  r = soup.find_all("a", class_="bookTitle")
-  print(r[0].string)
+  table = soup.find_all("a", class_="bookTitle")
+  data = []
+  for e in table:
+    title = e.get_text(strip=True)
+    data.append({"title": title})
+
+  df = pd.DataFrame(data)
+  print(df.sample(n=20))
 
 if __name__ == '__main__':
   main()

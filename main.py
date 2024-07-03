5273261262be2cee703ed6093649675b453510d1
eugsa <eugenie.saliou@gmail.com>
2024-07-03 11:17:55 +0200

    Add collecting books titles and additional text into a dataframe

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from bs4 import BeautifulSoup
 URL = "https://www.goodreads.com/shelf/show/non-fiction"
 
 def main():
-  # dfs = pd.read_html(URL)
-  # print(dfs)
-
   page = urlopen(URL)
   html_bytes = page.read()
   html = html_bytes.decode("utf-8")
-
   soup = BeautifulSoup(html, "html.parser")
-  print(soup.title.string)
 
-  table = soup.find_all("a", class_="bookTitle")
+  books_table = soup.find_all("div", class_="elementList")
+
   data = []
-  for e in table:
-    title = e.get_text(strip=True)
-    data.append({"title": title})
+  for e in books_table:
+    title = e.find("a", class_="bookTitle")
+    additional_text = e.find("span", class_="greyText smallText")
+
+    if title is not None:
+      book = {
+        "title": title.text,
+        "additional_text": additional_text.text
+      }
+      data.append(book)
 
   df = pd.DataFrame(data)
   print(df.sample(n=20))

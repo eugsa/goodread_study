d925113e155e9d73785c23a0a90f032c03471627
eugsa <eugenie.saliou@gmail.com>
2024-07-08 11:37:04 +0200

    Change for Selenium, login and access page

--- get_book_list.py
+++ get_book_list.py
@@ -1,7 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import re
+import pdb
+import os
+from dotenv import load_dotenv
 
 HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -12,6 +19,26 @@ URL = "https://www.goodreads.com/shelf/show/non-fiction"
 PAGE_URL_TEXT = "?page="
 
 REPORTS_PATH = "./reports/"
+
+def log_into_goodreads(driver):
+  login_url = "https://www.goodreads.com/user/sign_in"
+  driver.get(login_url)
+
+  access_sign_in_btn = driver.find_element(By.CLASS_NAME, "authPortalSignInButton")
+  access_sign_in_btn.click()
+  load_dotenv()
+  email_field = driver.find_element(By.ID, "ap_email")
+  password_field = driver.find_element(By.ID, "ap_password")
+  email_field.send_keys(os.getenv("GR_EMAIL"))
+  password_field.send_keys(os.getenv("GR_PASSWORD"))
+  sign_in_btn = driver.find_element(By.ID,"signInSubmit")
+  sign_in_btn.click()
+
+def selenium_request():
+  driver = webdriver.Chrome()
+  log_into_goodreads(driver)
+  driver.get(URL)
+  driver.quit()
 
 def soup_init():
   response = requests.get(URL, headers=HEADERS)
@@ -68,11 +95,13 @@ def generate_report(book_list):
   print(f"See saved report as { filepath }")
 
 def main():
-  soup = soup_init()
-  book_list = extract(soup)
-  clean_book_list = clean(book_list)
-  generate_report(clean_book_list)
-  print(clean_book_list.head(5))
+  selenium_request()
+
+  # soup = soup_init()
+  # book_list = extract(soup)
+  # clean_book_list = clean(book_list)
+  # generate_report(clean_book_list)
+  # print(clean_book_list.head(5))
 
 if __name__ == '__main__':
   main()

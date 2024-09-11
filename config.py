2aeb0cd70874fab16923df6da2d84bcb6141c63f
eugsa <eugenie.saliou@gmail.com>
2024-09-11 14:56:56 +0200

    Load book data to database

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 """Defining constant variables."""
+# URLs
 BASE_URL = "https://www.goodreads.com"
 LOGIN_URL = BASE_URL + "/user/sign_in"
 URL = BASE_URL + "/shelf/show/non-fiction"
 PAGE_URL_TEXT = "?page="
 REPORTS_PATH = "./reports/"
 
-BOOKS_FILENAME = "book_list.csv"
+# Database
+DATABASE = "goodreads_study"
+BOOKS_TABLE = "books"
 
+# Filenames and filapaths
+BOOKS_FILENAME = "book_list.csv"
 PRIZES_FILEPATH = "./data/"
 PULITZER_RAW_FILENAME = "pulitzer_prizes_raw.txt"
 PULITZER_CLEAN_FILENAME = "pulitzer_prizes.csv"
--- get_book_list.py
+++ get_book_list.py
@@ -4,6 +4,7 @@ import parser_utils
 import extract
 import transform
 import export
+import manage_db
 
 def main():
     arg_parser = argparse.ArgumentParser(
@@ -25,11 +26,18 @@ def main():
               in the script in order to continue the process.
         """)
 
+    # Get data, clean and transform it
     driver = parser_utils.selenium_request(debug)
     book_list = extract.extract_data(driver, debug)
     clean_book_list = transform.clean(book_list)
-    export.generate_report(clean_book_list)
     driver.quit()
+
+    # Generate CSV report
+    export.generate_report(clean_book_list)
+
+    # Load data to a database
+    manage_db.init_db()
+    manage_db.load_books_into_db(clean_book_list)
 
 if __name__ == '__main__':
     main()

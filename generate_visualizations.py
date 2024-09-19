fac0eb3825b39ac10e481066dde55937b2eaf3b7
eugsa <eugenie.saliou@gmail.com>
2024-09-19 10:27:11 +0200

    Add visualization to compare non-fiction and dnf popularity tag ranking

--- generate_visualizations.py
+++ generate_visualizations.py
@@ -4,6 +4,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import config
 
+def tag_ranking(dnf_df, nonfic_in_dnf):
+    new_df = nonfic_in_dnf[[ "title"]]
+    result_df = new_df.copy()
+    result_df["nonfic_ranking"] = result_df.index
+    dnf_df_reset = dnf_df.reset_index()
+    result_df = result_df.merge(dnf_df_reset[['title', 'index']], on='title', how='left')
+    result_df.rename(columns={'index': 'dnf_ranking'}, inplace=True)
+    
+    sns.set_theme(style="whitegrid")
+    sns.scatterplot(data=result_df, x="dnf_ranking", y="nonfic_ranking")
+    plt.show()
+
 def books_in_dnf(nonfic_df, nonfic_in_dnf):
     count = {
         "count_nonfic_total": len(nonfic_df),
@@ -20,6 +32,7 @@ def visualize_data(nonfic_books_csv, dnf_books_csv):
     nonfic_in_dnf = nonfic_df[nonfic_df.title.isin(dnf_df.title)]
 
     books_in_dnf(nonfic_df, nonfic_in_dnf)
+    tag_ranking(dnf_df, nonfic_in_dnf)
 
 def main():
     nonfic_books_csv = config.REPORTS_PATH + config.NONFIC_FILENAME
